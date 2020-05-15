879167a664b5ae41467cdaaf314df416bf9eb25c
Jun Villa <junarvi@yahoo.ca>
2020-05-15 11:07:02 -0400

    fix osu memory reader by changing build to x64

--- MainForm.cs
+++ MainForm.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using OsuMemoryDataProvider;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,16 @@ namespace circle_tracker
 {
     public partial class MainForm : Form
     {
+        private readonly Tracker tracker;
         public MainForm()
         {
             InitializeComponent();
+            tracker = new Tracker();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            tracker.OnClosing();
         }
     }
 }
--- Tracker.cs
+++ Tracker.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace circle_tracker
@@ -10,10 +11,41 @@ namespace circle_tracker
     class Tracker
     {
         private readonly IOsuMemoryReader osuReader;
+        private Thread thread;
+        private int updateInterval = 33;
+        private bool running;
+
+        // osu temporal variables variables
+        private string cachedBeatmapFilename;
 
         public Tracker()
         {
             osuReader = OsuMemoryReader.Instance.GetInstanceForWindowTitleHint("");
+            running = true;
+            thread = new Thread(TickLoop);
+            thread.Start();
+        }
+
+        public void OnClosing()
+        {
+            running = false;
+            thread?.Join();
+            Console.WriteLine("closing");
+        }
+
+        public void TickLoop()
+        {
+            while (running)
+            {
+                int status;
+                osuReader.GetCurrentStatus(out status);
+                string currentBeatmapFilename = osuReader.GetOsuFileName();
+                if (currentBeatmapFilename != cachedBeatmapFilename)
+                {
+                    Console.WriteLine(currentBeatmapFilename);
+                }
+                cachedBeatmapFilename = currentBeatmapFilename;
+            }
         }
 
         private decimal oppaiStars(string beatmapPath)
