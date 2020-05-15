720eda598ce0569f4a35e91907c5b9647d2c3e94
Jun Villa <junarvi@yahoo.ca>
2020-05-14 23:03:27 -0400

    add submodules

---
+++ Tracker.cs
@@ -0,0 +1,29 @@
+﻿using OsuMemoryDataProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circle_tracker
+{
+    class Tracker
+    {
+        private readonly IOsuMemoryReader osuReader;
+
+        public Tracker()
+        {
+            osuReader = OsuMemoryReader.Instance.GetInstanceForWindowTitleHint("");
+        }
+
+        private decimal oppaiStars(string beatmapPath)
+        {
+            (decimal stars, decimal aim, decimal speed) = oppai(beatmapPath);
+            return stars;
+        }
+        private (decimal, decimal, decimal) oppai(string beatmapPath)
+        {
+            return (0, 0, 0);
+        }
+    }
+}
