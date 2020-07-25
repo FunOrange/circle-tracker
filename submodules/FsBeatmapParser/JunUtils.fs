6aee1e5249158074da72489745ffde54d619f6f8
Jun Villa <junarvi@yahoo.ca>
2020-07-25 16:48:53 -0400

    Fixed guess who is back

--- submodules/FsBeatmapParser/JunUtils.fs
+++ submodules/FsBeatmapParser/JunUtils.fs
@@ -53,15 +53,20 @@ let removeOuterQuotes (s:string) =
     | _ -> s
 
 let split separator (s:string) =
+    let quoteCount = (Seq.filter ((=) '"') s) |> Seq.length
+    let fixedStr =
+        match quoteCount % 2 with
+        | 1 -> s.Replace("\"", "")
+        | _ -> s
     let values = ResizeArray<_>()
     let rec gather start i =
-        let add () = s.Substring(start,i-start) |> values.Add
-        if i = s.Length then add()
-        elif s.[i] = '"' then inQuotes start (i+1) 
-        elif s.[i] = separator then add(); gather (i+1) (i+1) 
+        let add () = fixedStr.Substring(start,i-start) |> values.Add
+        if i = fixedStr.Length then add()
+        elif fixedStr.[i] = '"' then inQuotes start (i+1) 
+        elif fixedStr.[i] = separator then add(); gather (i+1) (i+1) 
         else gather start (i+1)
     and inQuotes start i =
-        if s.[i] = '"' then gather start (i+1)
+        if fixedStr.[i] = '"' then gather start (i+1)
         else inQuotes start (i+1)
     gather 0 0
 
