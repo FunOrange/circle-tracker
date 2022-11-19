462d34c3cce7839b42287132974c3c9dcfb4c8b7
FunOrange <junarvi@yahoo.ca>
2022-11-19 14:08:17 -0500

    fix song detection being broken since apr. 2022

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Please watch this video for more information:
 https://www.youtube.com/watch?v=_65AvmAjlpY
 
 TODO: add more stuff here
+
+## Build troubleshooting
+Make sure all projects are set to build in Configuration Manager
+https://www.primordialcode.com/blog/post/referenced-project-targeted-different-framework-family
--- Updater.cs
+++ Updater.cs
@@ -101,7 +101,7 @@ namespace Circle_Tracker
 
     class Updater
     {
-        static readonly string CURRENT_RELEASE_TAG = "v14"; // Jun: REMEMBER TO CHANGE THIS every time you make a new release.
+        static readonly string CURRENT_RELEASE_TAG = "v16"; // Jun: REMEMBER TO CHANGE THIS every time you make a new release.
         static HttpClient client; 
         static Updater()
         {
